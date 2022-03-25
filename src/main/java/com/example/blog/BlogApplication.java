67a84574f936a4b71015a4d9c113c53a4020c2c9
Quoc <qnguyen523@gmail.com>
2022-03-25 21:22:25 +0700

    set up index page

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -1,13 +1,25 @@
 package com.example.blog;
 
+import org.springframework.boot.*;
+// import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BlogApplication {
-
+@RestController
+public class BlogApplication implements ApplicationRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
-
+	@RequestMapping(value = "/")
+   	public String hello() {
+      	return "I love you";
+	}
+	@Override
+	// execute the code after the Spring Boot application started
+   	public void run(ApplicationArguments arg0) throws Exception {
+    	System.out.println("Hello World from Application Runner");
+   	}
 }
